57db29fd131f354d0fc5c9e76f5aa05b2df34c06
Jon Zeppieri <jon.zeppieri@meyouhealth.com>
2015-03-21 19:13:35 -0400

    initial commit

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define collection 'multi)
+(define deps '("base"
+               "cldr-core"
+               "json"))
+
+(define pkg-desc "CLDR core")
+
+(define pkg-authors '(97jaz))
